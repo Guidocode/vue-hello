ffeb578b4a23399d0790595c95eaabf9a7a9fee4
Guidocode <guidobuono@icloud.com>
2022-05-02 17:09:42 +0200

    Stampo hello in pagina con vue.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+  **Descrizione:**
+  Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+  **Bonus:**
+  Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+
+let app = new Vue({
+
+  el: '#app',
+
+  data: {
+    message: 'Hello',
+    color: 'white',
+    weight: 'bold', 
+    size: 'size',
+  }
+})
+
+
+
+
